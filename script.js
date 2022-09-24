353d591a9f045f0755b9193efeb7f150353e636a
Braco <isovicramiz07@gmail.com>
2022-09-24 12:48:52 -0400

    basic functionality no gui

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operator(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-': 
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break
+    }
+}
